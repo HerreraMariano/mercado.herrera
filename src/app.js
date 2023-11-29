b24cb065cfd1d0f6d81e2438c28fa07a84a2e74c
HerreraMariano <herreramariano.ok@gmail.com>
2023-11-29 02:11:16 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -11,9 +11,12 @@ app.use(express.static(publicPath));
 
 
 // poner a oir al servidor
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log('servidor corriendo en: http://localhost:3000/');
-});
+});*/
+// levantar el servidor
+const port = process.env.PORT || 3001;
+app.listen(port,() => console.log('servidor corriendo en el puerto $(port)'));
 
 
 app.get('/', (req, res) => {
@@ -21,4 +24,10 @@ app.get('/', (req, res) => {
 })
 app.get('/registro', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/registro.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+app.post('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
